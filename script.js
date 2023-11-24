5e4a8859ceeb25310961d2d552f9f67c070729c4
Jonasi-Deetens <jonasideetens@hotmail.com>
2023-11-24 09:37:02 +0100

    Add script that generates a button for each cocktail

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 import cocktails from "./cocktails.js";
 
-console.log(cocktails);
+const generateButtons = () => {
+  const cocktailsElement = document.querySelector("#cocktails");
+
+  cocktails.forEach((cocktail) => {
+    const button = document.createElement("button");
+    button.classList.add("cocktail-button");
+    button.textContent = cocktail.name + " - " + cocktail.price;
+    button.style.backgroundColor = cocktail.color;
+    button.addEventListener("click", () => {
+        console.log(cocktail.type === "cocktail" ? "a cocktail" : "not a cocktail" );
+    });
+    cocktailsElement.appendChild(button);
+  });
+};
+
+generateButtons();
